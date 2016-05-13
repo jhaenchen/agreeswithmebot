56827cac0b4ae07848e5e14ccc576080639e2e18
Jackson <jacksonhaenchen@gmail.com>
2016-05-13 00:54:37 +0000

    Now pulls by username mention. Also support for parent commenting. New formatted signature. Updated README

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # agreeswithmebot
-A reddit python bot to agree with you. Man's best friend.
+
+
+A reddit python bot to agree with you. Man's best friend for those pesky internet debates.
+
+Call it in comments with: "/u/agreeswithmebot" to agree with you.
+
+Call it in comments with "/u/agreeswithmebot that" to agree with your parent, in case you want to agree but don't want to have come up with something.
--- agreesWithMeBotV1.py
+++ agreesWithMeBotV1.py
@@ -15,24 +15,35 @@ agreePhrases=[
 	'Brilliant. Simply brilliant.',
 	'This mans name? Albert Einstein.',
 	'Damn. Couldnt have said it better my self.',
-	'Whoa, who said that, Shakespeare?']
+	'Whoa, who said that, Shakespeare?',
+	'Yeah, you tell him!',
+	'Honestly it\'s comments like these that make me smile.',
+	'That\'s incredible! Literally amazing!',
+	'What\'s it like being so amazing?']
 
+appendPhrase = '\n\n^(Need some backup?) [^"/u/agreeswithmebot"](https://github.com/jhaenchen/agreeswithmebot)'
 while True:
 	try:
-		user = r.get_redditor('therealjakeh')
-		for thing in user.get_comments():
-			op_text = thing.body.lower()
-        		has_praw = any(string in op_text for string in prawWords)
-        # Test if it contains a PRAW-related question
-			if thing.id not in already_done and has_praw:
-            			thing.reply(agreePhrases[random.randrange(0,len(agreePhrases))])
-            			already_done.append(thing.id)
-				print "Detected a post. Posting comment..."
+		print "checking..."
+		#Check my messages
+		for message in r.get_unread(unset_has_mail=True, update_user=True):
+			if("!agree" in message.body.lower() or  "/u/agreeswithmebot" in message.body.lower()):
+
+				if ("that" in message.body.lower()):
+					print "Got child  message, sending parent reply"
+					parent = r.get_info(thing_id=message.parent_id)
+					#Respond with a random sentence from the available collection
+					parent.reply(agreePhrases[random.randrange(0,len(agreePhrases))]+appendPhrase)
+				else:
+					print "Got message, sending reply"
+					message.reply(agreePhrases[random.randrange(0,len(agreePhrases))]+appendPhrase)
+			message.mark_as_read()
+		print "sleeping..."
 		time.sleep(210)
 	except praw.errors.HTTPException:
 		print "Http exception. Will try again."
 	except praw.errors.RateLimitExceeded as error:
-            	print '\tSleeping for %d seconds' % error.sleep_time
+		print '\tSleeping for %d seconds' % error.sleep_time
             	time.sleep(error.sleep_time)		
 	except praw.errors.APIException:
 		print "API exception. Will try again."

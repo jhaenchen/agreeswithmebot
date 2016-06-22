3f622314e10cb64df5b3ef2479e80f176ad70b44
Jackson <jacksonhaenchen@gmail.com>
2016-06-22 04:51:02 +0000

    Quoting makes things better. For now it's always the second sentence but soon I'll figure out how to randomly select a sentence.

--- agreesWithMeBotV1.py
+++ agreesWithMeBotV1.py
@@ -1,7 +1,7 @@
 import praw
 import time
 import random
-import pprint
+from pprint import pprint
 import requests
 r = praw.Reddit('agreeswithme'
 		'Url:http://imtoopoorforaurl.com')
@@ -13,6 +13,9 @@ prawWords = ['!agree']
 
 
 agreePhrases=[
+	'I\'m unsure of how you were able to craft this masterpiece, but simply saying that I agree seems shallow and unsophisticated in comparison. You speak to what is in all of our hearts.',
+	'You know, you\'re right. Here, have an upvote on me!',
+	'That\'s really sharp! I couldn\'t agree more.',
 	'Whoa, that makes so much sense. Thank you, sensei.',
 	'Brilliant. Simply brilliant.',
 	'This mans name? Albert Einstein.',
@@ -26,46 +29,74 @@ agreePhrases=[
 	'This made me start believing in god again. Amen.',
 	'I\'ve been on this earth forty years and only now just realized what I\'ve been missing: your words.',
 	'This whole bot thing is overrated. I\'m just going to keep printing your comment forever instead. Because it\'s awesome.',
-	'Pretty. damn. smart.'
+	'Pretty. damn. smart.',
+	'Fo shizzle ma dawg.',
+	'I think I saw that carved into some Mayan ruins. Amazing wisdom from a forgotten age.',
 	'My mom saw this and said she\'s sending you cookies. Thank you.',
 	'How do you always manage to fit so much meaning into so few words?',
 	'Deeper than the marina trench.',
 	'God? Is that you?',
-	'I\'m working on another bot just to keep up with how much I agee with you.',
+	'I\'m working on another bot just to keep up with how much I agree with you.',
 	'How do you spell that? Because Im getting a tattoo of it.',
 	'Ah yes. Mark 23:12. A wise verse.']
 
 appendPhrase = '\n\n^(Need some backup?) [^"/u/agreeswithmebot"](https://github.com/jhaenchen/agreeswithmebot)'
-appendPhrase = '\n\n *** \n^^(Need) ^^(backup?) ^[^"/u/agreeswithmebot"!](https://reddit.com/u/agreeswithmebot)\n\n^[^Contribute](https://www.reddit.com/r/agreeswithmebot/submit?text=%20&title=Agree:%3Cyour%20text%20here%3E) ^[^an](https://www.reddit.com/r/agreeswithmebot/submit?text=%20&title=Agree:%3Cyour%20text%20here%3E) ^[^agreement](https://www.reddit.com/r/agreeswithmebot/submit?text=%20&title=Agree:%3Cyour%20text%20here%3E) ^^(|) ^[^Github](https://github.com/jhaenchen/agreeswithmebot) ^^(|) ^[^Subreddit](https://reddit.com/r/agreeswithmebot)'
+#appendPhrase = '\n\n *** \n^^(Need) ^^(backup?) ^[^"/u/agreeswithmebot"!](https://reddit.com/u/agreeswithmebot)\n\n^[^Contribute](https://www.reddit.com/r/agreeswithmebot/submit?text=%20&title=Agree:%3Cyour%20text%20here%3E) ^[^an](https://www.reddit.com/r/agreeswithmebot/submit?text=%20&title=Agree:%3Cyour%20text%20here%3E) ^[^agreement](https://www.reddit.com/r/agreeswithmebot/submit?text=%20&title=Agree:%3Cyour%20text%20here%3E) ^^(|) ^[^Github](https://github.com/jhaenchen/agreeswithmebot) ^^(|) ^[^Subreddit](https://reddit.com/r/agreeswithmebot)'
+
+#appendPhrase = '\n\n *** \n^^(Need) ^^(backup?) ^[^"/u/agreeswithmebot"!](https://reddit.com/r/agreeswithmebot) ^^|| ^^[Add](https://www.reddit.com/r/agreeswithmebot/submit?text=%20&title=Agree:%3Cyour%20text%20here%3E) ^^an ^^agreement'
+
+def generateQuote(message):
+	quote = "."
+	if("." in message.body):
+		indexOf = message.body.index(".")
+		endQuote = len(message.body)
+		substring = message.body[indexOf+1:]
+		if("." in substring):
+			endQuote = substring.index(".")
+		quote = "\n>" + message.body[indexOf+1:indexOf+endQuote+1]+"\n\n"
+	return quote;
+
 while True:
 	try:
 		print "checking..."
 		#Check my messages
 		for message in r.get_unread(unset_has_mail=True, update_user=True):
 			if("!agree" in message.body.lower() or  "/u/agreeswithmebot" in message.body.lower()):
-
 				if ("!parent" in message.body.lower()):
 					print "Got child  message, sending parent reply"
 					parent = r.get_info(thing_id=message.parent_id)
 					if(isinstance(parent, praw.objects.Comment)):
-						parent.reply(agreePhrases[random.randrange(0,len(agreePhrases))]+appendPhrase)
+						quote = generateQuote(parent)
+						print quote
+						parent.reply(quote+agreePhrases[random.randrange(0,len(agreePhrases))]+appendPhrase)
 					elif(isinstance(parent, praw.objects.Submission)):
 						parent.add_comment(agreePhrases[random.randrange(0,len(agreePhrases))]+appendPhrase)
 					else:
 						message.reply(agreePhrases[random.randrange(0,len(agreePhrases))]+appendPhrase)
 				else:
 					print "Got message, sending reply"
-					message.reply(agreePhrases[random.randrange(0,len(agreePhrases))]+appendPhrase)
+					quote = generateQuote(message)
+					print quote
+					message.reply(quote+agreePhrases[random.randrange(0,len(agreePhrases))]+appendPhrase)
 			message.mark_as_read()
 		print "sleeping..."
 		time.sleep(210)
 	except requests.exceptions.ReadTimeout:
-		"Read timeout. Will try again."
-	except praw.errors.HTTPException:
+		print "Read timeout. Will try again."
+	except praw.errors.Forbidden:
+		print "Im banned from there."
+		user = message.author
+		r.send_message(user.name, 'AgreesWithMeBot', 'Hey, I\'m banned from there. Sorry.')
+		message.mark_as_read()
+	except praw.errors.HTTPException as e:
+		pprint(vars(e))	
+		print(e)
 		print "Http exception. Will try again."
 	except praw.errors.RateLimitExceeded as error:
 		print '\tSleeping for %d seconds' % error.sleep_time
             	time.sleep(error.sleep_time)		
+	except requests.exceptions.ConnectionError:
+		print "ConnectionError. Will try again."
 	except praw.errors.APIException:
 		print "API exception. Will try again."
 	except (KeyboardInterrupt, SystemExit):
